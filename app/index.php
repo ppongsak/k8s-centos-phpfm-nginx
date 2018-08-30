ce6e4fdaeee65a5b8a2e81fcf46a87fb12151955
Pongsak Prabparn <pongsak@Pongsaks-MacBook-Pro.local>
2018-08-30 11:48:48 +0700

    edit mysql

--- app/index.php
+++ app/index.php
@@ -6,11 +6,19 @@ echo "======================================================================";
 echo "</pre>";
 
 echo "connecting to mysql";
+echo "<pre></pre>";
 
 
 $servername = getenv('DEV_DB_HOST');
 $username = getenv('DB_USER');
 $password = getenv('DB_PASSWORD');
+
+echo $servername;
+echo "<pre></pre>";
+echo $username;
+echo "<pre></pre>";
+echo $password;
+echo "<pre></pre>";
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
